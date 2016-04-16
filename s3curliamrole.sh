5860dbda4cb6ecf8fddff2c55ca88f8f8eb0bc1a
Peter Mescalchin <peter@magnetikonline.com>
2016-04-16 17:43:44 +1000

    Added quotes to a few more arguments for safety

--- s3curliamrole.sh
+++ s3curliamrole.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-DIRNAME=$(dirname $0)
+DIRNAME=$(dirname "$0")
 IAM_BASE_URL="http://169.254.169.254/latest/meta-data/iam/security-credentials"
 S3CURL_PROFILE="iamrole"
 CONFIG_FILE="$HOME/.s3curl"
@@ -8,8 +8,8 @@ CONFIG_FILE="$HOME/.s3curl"
 
 if [[ ! -f $CONFIG_FILE ]]; then
 	# fetch machine IAM role key/token details
-	IAMRoleName=$(curl -s $IAM_BASE_URL/)
-	IAMRoleData=$(curl -s $IAM_BASE_URL/$IAMRoleName/)
+	IAMRoleName=$(curl -s "$IAM_BASE_URL/")
+	IAMRoleData=$(curl -s "$IAM_BASE_URL/$IAMRoleName/")
 	IAMRoleAccessKeyID=$(echo -n "$IAMRoleData" | sed -nr 's/.*?"AccessKeyId"[^"]+"([^"]+)",?/\1/p')
 	IAMRoleAccessKeySecret=$(echo -n "$IAMRoleData" | sed -nr 's/.*?"SecretAccessKey"[^"]+"([^"]+)",?/\1/p')
 	IAMRoleToken=$(echo -n "$IAMRoleData" | sed -nr 's/.*?"Token"[^"]+"([^"]+)",?/\1/p')
@@ -26,7 +26,7 @@ if [[ ! -f $CONFIG_FILE ]]; then
 	chmod 0600 $CONFIG_FILE
 else
 	# extract token from config file
-	IAMRoleToken=$(cat $CONFIG_FILE | sed -nr "s/.*?token[^']+'([^']+)',?/\1/p")
+	IAMRoleToken=$(cat "$CONFIG_FILE" | sed -nr "s/.*?token[^']+'([^']+)',?/\1/p")
 fi
 
 # call s3curl.pl with token and any given arguments
